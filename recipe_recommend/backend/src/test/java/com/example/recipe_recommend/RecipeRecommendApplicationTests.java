d147276f1332c68db0008c2784b88727b588f78c
enott113 <5519sin@gmail.com>
2026-05-25 12:15:02 +0900

    Add files via upload

---
+++ recipe_recommend/backend/src/test/java/com/example/recipe_recommend/RecipeRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
